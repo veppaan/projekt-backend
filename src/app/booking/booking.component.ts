9cc4bc169a322fad6719d8d5f9b60a8e489cc2a7
Vera Kippel <veki2400@student.miun.se>
2025-05-22 23:00:30 +0200

    justerar knappar och funktion vid klick

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss'
 })
 export class BookingComponent {
-
+  starter: string= "";
+  main: string= "";
+  dessert: string= "";
+  name: string="";
+  number: string = "";
   starters: any = [];
   mainCourses: any = [];
   desserts: any = [];
@@ -25,6 +30,18 @@ export class BookingComponent {
     this.http.get("https://backend-projekt-api-jxss.onrender.com/api/desserts").subscribe(data => {
       this.desserts = data;
     });
+  }
+
+  payText = "Beställ och betala 89kr";
+
+  payButton(){
+
+    this.starter="";
+    this.main="";
+    this.dessert="";
+    this.name="";
+    this.number="";
+    alert("Tack för din beställning! Du får ett sms inom 10 min att den är färdig att hämtas upp!");
   }
 
 }
