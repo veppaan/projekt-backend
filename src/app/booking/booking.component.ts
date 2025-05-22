830315a1f10478dc41c08c1d8d01d65c142a1fba
Vera Kippel <veki2400@student.miun.se>
2025-05-22 19:24:49 +0200

    hämtar urler och lägger in datan till sida

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss'
 })
 export class BookingComponent {
+  private url: string = "https://backend-projekt-api-jxss.onrender.com"
+  starters: any = [];
+  mainCourses: any = [];
+  desserts: any = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://backend-projekt-api-jxss.onrender.com/api/starters").subscribe(data => {
+      this.starters = data;
+    });
+    this.http.get("https://backend-projekt-api-jxss.onrender.com/api/main-courses").subscribe(data => {
+      this.mainCourses = data;
+    });
+    this.http.get("https://backend-projekt-api-jxss.onrender.com/api/desserts").subscribe(data => {
+      this.desserts = data;
+    });
+  }
 
 }
