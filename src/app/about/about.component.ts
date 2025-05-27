8d07c56ddedecd1601219ffbffc14715a68161c9
Vera Kippel <veki2400@student.miun.se>
2025-05-27 16:24:32 +0200

    Skapar information på aboutsida

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  dog: string = "images/about-dog.webp"
 }
