da84ec1716a51f48f7bb159eaeec667d57a9194e
Vera Kippel <veki2400@student.miun.se>
2025-05-27 15:09:10 +0200

    laddar ner google maps paket och lägger in karta

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, GoogleMapsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   homePic: string = "images/dog2.webp"
+
+  center: google.maps.LatLngLiteral = { lat: 59.331250, lng: 18.071417 }
+  marker: google.maps.LatLngLiteral = { lat: 59.331250, lng: 18.071417 };
 }
